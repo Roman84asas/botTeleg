86e4e9c5624d23b05f8ba96435e2283500be07a6
RomanMereneanu <asasin84@mail.ru>
2019-09-20 19:42:30 +0300

    add proj

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,6 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
+
+        if ($this->app->environment() !== 'local') {
+            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+        }
         //
     }
 
